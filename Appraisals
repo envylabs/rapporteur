1af36ef3a5fecab7afbc2ea592cf1fcbe08ba684
Nathaniel Bibler <nate@envylabs.com>
2013-05-19 16:49:46 -0400

    Support Rails 3.1. Add Appraisals for multi-Rails testing.

---
+++ Appraisals
@@ -0,0 +1,7 @@
+appraise "rails3.1" do
+  gem "railties", "~> 3.1.0"
+end
+
+appraise "rails3.2" do
+  gem "railties", "~> 3.2.0"
+end
--- lib/codeschool/status/responder.rb
+++ lib/codeschool/status/responder.rb
@@ -13,8 +13,14 @@ module Codeschool
       protected
 
 
-      def display_errors
-        controller.render format => resource_errors, status: :internal_server_error
+      if Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR == 1
+        def display_errors
+          controller.render format => {errors: resource.errors}, status: :internal_server_error
+        end
+      elsif Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR == 2
+        def display_errors
+          controller.render format => resource_errors, status: :internal_server_error
+        end
       end
     end
   end
