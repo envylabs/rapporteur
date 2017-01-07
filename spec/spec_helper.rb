2c0c5c211c9e34b992fd4873d2a444942e9a6e6c
Nathaniel Bibler <nate@envylabs.com>
2017-01-07 18:21:25 -0500

    Remove Ruby 1.9.3, Rails 3.2 Test::Unit shim.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,8 +17,3 @@ RSpec.configure do |config|
 
   config.before { Rapporteur.clear_checks }
 end
-
-
-# This is a shim to allow for Ruby 1.9.3, Rails 3.2 testing to pass.
-# See https://github.com/rspec/rspec-rails/issues/1171.
-Test::Unit.run = true if defined?(Test::Unit) && Test::Unit.respond_to?(:run=)
